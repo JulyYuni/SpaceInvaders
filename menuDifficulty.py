af3f68cd132ac02e59644a5c85236e65fe7def52
JulyYuni <jujucc_2003@hotmail.com>
2024-06-16 23:56:25 -0300

    Comentários organizados

--- menuDifficulty.py
+++ menuDifficulty.py
@@ -6,7 +6,7 @@ from functions import *
 
 '''''''''''FUNÇÕES'''''''''
 
-#7 Menu de dificuldade (função que altera velocidade da nave e do tiro)
+#1 Menu de dificuldade (função que altera velocidade da nave e do tiro)
 def menu_dificuldade(gamestate, inicializou_gamestate):
 
     while gamestate == 2:
@@ -16,13 +16,13 @@ def menu_dificuldade(gamestate, inicializou_gamestate):
             #Gera o cenário
             posicoes()
             gerando_cenario(gamestate)
-            inicializou_gamestate = True         #Confirma que cenário foi gerado
+            inicializou_gamestate = True         # Confirma que cenário foi gerado
 
             #Inicializando variáveis que a função irá retornar
             velocidade_nave = 0
             velocidade_tiro = 0
 
-            #
+            #Nenhum botão foi clickado ainda
             clickou = 0
 
         
@@ -35,13 +35,13 @@ def menu_dificuldade(gamestate, inicializou_gamestate):
         #se o botao já foi clickado, mas o usuário soltou o click
         if click_left == False and clickou > 0:
 
-            if clickou == 1:
+            if clickou == 1:            # Fácil
                 velocidade_nave = 500
                 velocidade_tiro = 500
-            elif clickou == 2:
+            elif clickou == 2:          # Médio
                 velocidade_nave = 250
                 velocidade_tiro = 250
-            elif clickou == 3:
+            elif clickou == 3:          # Difícil
                 velocidade_nave = 100
                 velocidade_tiro = 100
             
